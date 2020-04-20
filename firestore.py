a7ff852bd77c4341dbaa6bbf0015f74605eae432
Christoffer Sundqvist <fiskmoz96@gmail.com>
2020-04-20 19:13:05 +0200

    more debugging in production

--- firestore.py
+++ firestore.py
@@ -24,6 +24,18 @@ if os.environ.get('project_id') is None:
 
 
 if os.environ.get('project_id') is not None:
+    print({
+        "type": os.environ.get('type'),
+        "project_id": os.environ.get('project_id'),
+        "private_key_id": os.environ.get('private_key_id'),
+        "private_key": os.environ.get('private_key'),
+        "client_email": os.environ.get('client_email'),
+        "client_id": os.environ.get('client_id'),
+        "auth_uri": os.environ.get('auth_uri'),
+        "token_uri": os.environ.get('token_uri'),
+        "auth_provider_x509_cert_url": os.environ.get('auth_provider_x509_cert_url'),
+        "client_x509_cert_url": os.environ.get('client_x509_cert_url'),
+    })
     cred = credentials.Certificate({
         "type": os.environ.get('type'),
         "project_id": os.environ.get('project_id'),
@@ -36,7 +48,6 @@ if os.environ.get('project_id') is not None:
         "auth_provider_x509_cert_url": os.environ.get('auth_provider_x509_cert_url'),
         "client_x509_cert_url": os.environ.get('client_x509_cert_url'),
     })
-    print(cred)
     firebase_admin.initialize_app(cred)
 
     db = firestore.client()
