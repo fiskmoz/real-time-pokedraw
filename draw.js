b36acb20375e0a36c63aa153031edd670424bfd3
Christoffer Sundqvist <fiskmoz96@gmail.com>
2020-04-21 00:12:32 +0200

    styling, refactoring and QOL

--- draw.js
+++ draw.js
@@ -9,45 +9,18 @@ let FILLED = {};
 let previousPixel = [0, 0];
 
 function init() {
-  context.strokeStyle = "rgba(0,0,0,0.1)";
-  for (let i = 0; i < DIMENSION; i++) {
-    x = Math.floor((i * WIDTH) / DIMENSION);
-    context.beginPath();
-    context.moveTo(x, 0);
-    context.lineTo(x, HEIGHT);
-    context.stroke();
+  clear();
 
-    y = Math.floor((i * HEIGHT) / DIMENSION);
-    context.beginPath;
-    context.moveTo(0, y);
-    context.lineTo(WIDTH, y);
-    context.stroke();
-  }
-
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyAWiqFCByM9LxsLDqq71YgVCpmXhqJTyNI",
-    authDomain: "real-time-pokedraw.firebaseapp.com",
-    databaseURL: "https://real-time-pokedraw.firebaseio.com",
-    projectId: "real-time-pokedraw",
-    storageBucket: "real-time-pokedraw.appspot.com",
-    messagingSenderId: "710326983039",
-    appId: "1:710326983039:web:5a8aa14aa69555db2c624a",
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  let db = firebase.firestore();
   db.collection("app")
     .doc(canvas.getAttribute("name"))
     .onSnapshot(function (doc) {
       let data = doc.data();
       for (let key in data) {
-        let coordinate = key.split(",");
         let pixelData = JSON.parse(data[key]);
         for (let subkey in pixelData["data"]) {
           let subcoordniate = subkey.split(",");
           let color = pixelData["data"][subcoordniate];
-          fillPixelFromFirestore(subcoordniate, color);
+          fillPixel(subcoordniate, color);
         }
       }
     });
@@ -56,26 +29,29 @@ function init() {
     el: "#picker",
     theme: "classic",
     comparison: false,
+    showAlways: false,
     swatches: [
+      "rgba(0,0,0,1)",
+      "rgba(255,255,255, 1)",
+      "rgba(255, 0, 0, 1)",
       "rgba(244, 67, 54, 1)",
-      "rgba(233, 30, 99, 0.95)",
-      "rgba(156, 39, 176, 0.9)",
-      "rgba(103, 58, 183, 0.85)",
-      "rgba(63, 81, 181, 0.8)",
-      "rgba(33, 150, 243, 0.75)",
-      "rgba(3, 169, 244, 0.7)",
-      "rgba(0, 188, 212, 0.7)",
-      "rgba(0, 150, 136, 0.75)",
-      "rgba(76, 175, 80, 0.8)",
-      "rgba(139, 195, 74, 0.85)",
-      "rgba(205, 220, 57, 0.9)",
-      "rgba(255, 235, 59, 0.95)",
+      "rgba(233, 30, 99, 1)",
+      "rgba(156, 39, 176, 1)",
+      "rgba(103, 58, 183, 1)",
+      "rgba(63, 81, 181, 1)",
+      "rgba(33, 150, 243, 1)",
+      "rgba(3, 169, 244, 1)",
+      "rgba(0, 188, 212, 1)",
+      "rgba(0, 150, 136, 1)",
+      "rgba(76, 175, 80, 1)",
+      "rgba(139, 195, 74, 1)",
+      "rgba(255, 235, 59, 1)",
       "rgba(255, 193, 7, 1)",
     ],
     components: {
       preview: true,
-      opacity: true,
-      hue: true,
+      opacity: false,
+      hue: false,
       palette: true,
       interaction: {
         input: true,
@@ -96,31 +72,25 @@ function init() {
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhr.send(JSON.stringify(data));
   };
+
+  window.clear_canvas = function () {
+    this.clear();
+  };
 }
 
 function fill(event) {
   if (event.which == 0) return;
+  if (event.srcElement.id != canvas.id) return;
   let pixel = [
     Math.floor(event.offsetX / PIXELSIZE),
     Math.floor(event.offsetY / PIXELSIZE),
   ];
   if (pixel[0] == previousPixel[0] && pixel[1] == previousPixel[1]) return;
-  fillPixel(pixel);
+  fillPixel(pixel, COLOR);
   previousPixel = pixel;
 }
 
-function fillPixel(pixel) {
-  FILLED[pixel[0] + "," + pixel[1]] = COLOR;
-  context.fillStyle = COLOR;
-  context.fillRect(
-    pixel[0] * PIXELSIZE,
-    pixel[1] * PIXELSIZE,
-    PIXELSIZE - 1,
-    PIXELSIZE - 1
-  );
-}
-
-function fillPixelFromFirestore(pixel, color) {
+function fillPixel(pixel, color) {
   FILLED[pixel[0] + "," + pixel[1]] = color;
   context.fillStyle = color;
   context.fillRect(
@@ -129,6 +99,25 @@ function fillPixelFromFirestore(pixel, color) {
     PIXELSIZE - 1,
     PIXELSIZE - 1
   );
+}
+
+function clear() {
+  FILLED = {};
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.strokeStyle = "rgba(0,0,0,0.1)";
+  for (let i = 0; i < DIMENSION; i++) {
+    x = Math.floor((i * WIDTH) / DIMENSION);
+    context.beginPath();
+    context.moveTo(x, 0);
+    context.lineTo(x, HEIGHT);
+    context.stroke();
+
+    y = Math.floor((i * HEIGHT) / DIMENSION);
+    context.beginPath;
+    context.moveTo(0, y);
+    context.lineTo(WIDTH, y);
+    context.stroke();
+  }
 }
 
 init();
--- index.php
+++ index.php
@@ -1,51 +1,26 @@
 <?php
 
+$css = file_get_contents('styling/main.css');
+$header = file_get_contents('components/header.html');
+$footer = file_get_contents('components/footer.html');
+
 print <<<EOF
 <!doctype html>
 <html>
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/7.14.1/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/7.14.1/firebase-firestore.js"></script>
-
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-
-<script>
-
-</script>
-
-<style>
-    body {
-        background-color: aliceblue;
-        margin : 16px;
-        font-family: Arial, Helvetica, sans-serif;
-    }
-    
-    #canvas_wrapper {
-        position: relative;
-    }
-
-    #preview_canvas {
-        border:1px #000000 solid; 
-        cursor: pointer
-    }
-
-    #selectedBox {
-        border : 1px #2b5e8a solid;
-        background-color : #4da0e8;
-        position: absolute;
-        pointer-events: none;
-    }
-
-</style>
-<body>
-
-<div id="canvas_wrapper">
-    <canvas id=preview_canvas></canvas>
-</div>
-
-<script src="index.js"></script>
-</body>
+    <style>
+        $css
+    </style>
+    <header>
+        $header
+    </header>
+    <body>
+        <div id="canvas_wrapper">
+            <canvas id=preview_canvas></canvas>
+        </div>
+    </body>
+    <footer>
+        $footer
+        <script src="index.js"></script>
+    </footer>
 </html>
-
 EOF;
