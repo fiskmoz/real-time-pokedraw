5cca6ae7775cc9efeecaff05502b00d76f1ec4a2
Christoffer Sundqvist <fiskmoz96@gmail.com>
2020-04-20 12:08:38 +0200

    slight performance boost

--- draw.js
+++ draw.js
@@ -1,10 +1,11 @@
 const canvas = document.getElementById("draw_canvas");
 const context = canvas.getContext("2d");
-let DIMENSION = 25;
+let DIMENSION = 35;
 let WIDTH = canvas.clientWidth;
 let HEIGHT = canvas.clientWidth;
 let PIXELSIZE = WIDTH / DIMENSION;
 let COLOR = "#42445A";
+let previousPixel = [0, 0];
 
 function init() {
   context.strokeStyle = "rgba(0,0,0,0.1)";
@@ -26,6 +27,22 @@ function init() {
     el: "#picker",
     theme: "classic",
     comparison: false,
+    swatches: [
+      "rgba(244, 67, 54, 1)",
+      "rgba(233, 30, 99, 0.95)",
+      "rgba(156, 39, 176, 0.9)",
+      "rgba(103, 58, 183, 0.85)",
+      "rgba(63, 81, 181, 0.8)",
+      "rgba(33, 150, 243, 0.75)",
+      "rgba(3, 169, 244, 0.7)",
+      "rgba(0, 188, 212, 0.7)",
+      "rgba(0, 150, 136, 0.75)",
+      "rgba(76, 175, 80, 0.8)",
+      "rgba(139, 195, 74, 0.85)",
+      "rgba(205, 220, 57, 0.9)",
+      "rgba(255, 235, 59, 0.95)",
+      "rgba(255, 193, 7, 1)",
+    ],
     components: {
       preview: true,
       opacity: true,
@@ -46,10 +63,13 @@ function init() {
 
 function fill(event) {
   if (event.which == 0) return;
-  fillPixel([
+  let pixel = [
     Math.floor(event.offsetX / PIXELSIZE),
     Math.floor(event.offsetY / PIXELSIZE),
-  ]);
+  ];
+  if (pixel[0] == previousPixel[0] && pixel[1] == previousPixel[1]) return;
+  fillPixel(pixel);
+  previousPixel = pixel;
 }
 
 function fillPixel(pixel) {
