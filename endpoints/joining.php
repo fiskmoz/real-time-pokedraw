8eb492d3569bf6e777611e818f4983d545440b9f
Christoffer Sundqvist <fiskmoz96@gmail.com>
2020-04-25 20:59:43 +0200

    endpoint structure and performance fixes

---
+++ endpoints/joining.php
@@ -0,0 +1,30 @@
+<?php
+
+$x = intval($_REQUEST['x']);
+$y = intval($_REQUEST['y']);
+$user = $_REQUEST['user'];    
+
+
+if (!isset($x))
+{
+    echo "User not set";
+    return;
+}
+
+if (!isset($y))
+{
+    echo "X not set";
+    return;
+}
+
+if (!isset($user))
+{
+    echo "Y not set";
+    return;
+}
+
+$result = shell_exec("python ../scripts/user_join.py $x  $y '$user' 2>&1 &");
+    if($result != 1 ){
+        print_r($result);
+        die();
+    }
